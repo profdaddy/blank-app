876e208936cb3acf6305560321690847d3c0666c
Brian K Smith <macdaddy_b@me.com>
2024-07-26 17:01:13 +0000

    added URL code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,29 @@
 import streamlit as st
+import re
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def make_downloadable_GDrive_URL():
+    download_string = "https://drive.google.com/uc?export=download&id="
+
+    file_id_regex = r"\/d\/(.*)\/"
+
+    file_id_match = re.findall(file_id_regex, original_URL_text)
+    print("Text:", original_URL_text)
+
+    if file_id_match != []:        
+        st.session_state["download_URL"] = download_string + file_id_match[0]
+
+        
+
+
+if "download_URL" not in st.session_state:
+   st.session_state.download_URL = ""
+
+def changeURL():
+   st.session_state.download_URL = "http:"
+
+original_URL_text = st.text_input("Shared GDrive URL:")
+st.button("Plus one!", on_click=make_downloadable_GDrive_URL)
+st.subheader("New URL")
+st.code(st.session_state.download_URL)
+
+
